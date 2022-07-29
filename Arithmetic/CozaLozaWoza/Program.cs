1d4380e0aca34dade8042c390773a5b702ef0714
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-07-29 18:21:15 +0300

    Exercise #6 - CozaLozaWoza

---
+++ Arithmetic/CozaLozaWoza/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozaLozaWoza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerNum = 1;
+            int upperNum = 110;
+            int numPerLine = 11;
+            string result = "";
+
+            for (int i = lowerNum; i <= upperNum; ++i)
+            {
+                if (i % 7 == 0 && i % 5 == 0 && i % 3 == 0)
+                {
+                    result += "CozaLozaWoza ";
+                }
+                else if (i % 7 == 0)
+                {
+                    result += "Woza ";
+                }
+                else if (i % 5 == 0)
+                {
+                    result += "Loza ";
+                }
+                else if (i % 3 == 0)
+                {
+                    result += "Coza ";
+                }
+                else
+                {
+                    result += i+" ";
+                }
+
+                if (i % numPerLine == 0)
+                {
+                    result += "\n";
+                }
+            }
+
+            Console.WriteLine(result.Trim());
+            Console.ReadKey();
+        }
+    }
+}
