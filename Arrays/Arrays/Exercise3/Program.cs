896adbb0bd565d0a79a4b810e4bbf383922f4550
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-03 17:34:24 +0300

    Exercise #3 - ArrayNumAverage

--- Arrays/Arrays/Exercise3/Program.cs
+++ Arrays/Arrays/Exercise3/Program.cs
@@ -1,24 +1,26 @@
-﻿namespace Exercise3
+﻿using System;
+
+namespace Exercise3
 {
     class Program
     {
-        //TODO: Write a C# program to calculate the average value of array elements.
         private static void Main(string[] args)
         {
             int[] numbers = {20, 30, 25, 35, -16, 60, -100};
 
-            /*
-            fixme - calculate sum of all array elements 
-            int sum = 0;
-            for (?){
-                
-            }
-            */
+            int numCount = 0;
+            double sum = 0;
 
-            /*
-            fixme - calculate average value 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+                numCount += 1;
+            }
+            
+            double average = Math.Round(sum/numCount, 2);
+            
             Console.WriteLine("Average value of the array elements is : " + average);
-            */
+            Console.ReadKey();
         }
     }
 }
