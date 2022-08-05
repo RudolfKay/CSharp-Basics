1e137cbe5c71fc44d2ea4ea10531f691b9855885
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-05 20:37:22 +0300

    Exercise #9 - FirstLetterToUpper

---
+++ Arrays/Arrays/FirstLetterToUpper/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLetterToUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "samuel", "MABELLE", "letitiA", "meridith", "mavis", "SEnAida", "lETTy" };
+
+            Console.WriteLine(string.Join(", ",CapMe(names)));
+            Console.ReadKey();
+        }
+
+        static string[] CapMe(string[] names)
+        {
+            string[] answer = new string[names.Length];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string thisName = "";
+
+                for (int x = 0; x < names[i].Length; x++)
+                {
+                    if (x == 0)
+                    {
+                        thisName += char.ToUpper(names[i][x]);
+                    }
+                    else
+                    {
+                        thisName += char.ToLower(names[i][x]);
+                    }
+                }
+
+                answer[i] += thisName;
+            }
+
+            return answer;
+        }
+    }
+}
