ab6e64a5898959c76397a0293b7951fc71a7755d
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-06 18:48:13 +0300

    Exercise #11 - FindingNemo

---
+++ Arrays/Arrays/FindingNemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FindingNemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindNemo("I am finding Nemo !");
+            FindNemo("Nemo is me");
+            FindNemo("I Nemo am");
+        }
+
+        static void FindNemo(string s)
+        {
+            string[] input = s.Split(' ');
+            bool nemoFound = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Equals("Nemo"))
+                {
+                    Console.WriteLine($"I found Nemo at {i+1}!");
+                    nemoFound = true;
+                    break;
+                }
+            }
+
+            if (!nemoFound)
+            {
+                Console.WriteLine("I can't find Nemo :(");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
