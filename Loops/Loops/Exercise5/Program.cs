2f7d0706f5098a28befedbdec9814c47278eb7a5
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-06 23:51:07 +0300

    Exercise #5 - BookIndex

---
+++ Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineLength = 30;
+
+            Console.WriteLine("Choose 1st word: ");
+            string wordOne = Console.ReadLine();
+            Console.WriteLine("Choose 2nd word: ");
+            string wordTwo = Console.ReadLine();
+
+            string separator = "";
+            int separatorLength = lineLength - wordOne.Length - wordTwo.Length;
+
+            for (int i = 0; i < separatorLength; i++)
+            {
+                separator += ".";
+            }
+
+            Console.WriteLine($"{wordOne}{separator}{wordTwo}");
+            Console.ReadKey();
+        }
+    }
+}
