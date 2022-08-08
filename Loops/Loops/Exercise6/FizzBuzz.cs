fe939476886713509abc28e11ab5d3948aabdf5c
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-08 18:37:42 +0300

    Exercise #6 - FizzBuzz

---
+++ Loops/Loops/Exercise6/FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Max value? ");
+            var input = int.Parse(Console.ReadLine());
+            int lineLength = 20;
+
+            for (int i = 1; i <= input; i++)
+            {
+                Console.Write(FizzOrBuzz(i)+" ");
+
+                if (i % lineLength == 0)
+                {
+                    Console.WriteLine("\n");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static string FizzOrBuzz(int number)
+        {
+            string result = number.ToString();
+
+            if (number % 3 == 0)
+            {
+                result = "Fizz";
+
+                if (number % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+            }
+            else if (number % 5 == 0)
+            {
+                result = "Buzz";
+            }
+
+            return result;
+        }
+    }
+}
