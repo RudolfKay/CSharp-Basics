fad184a2cfac23503e1d89a0f461b427af2c6276
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-08 20:06:32 +0300

    Exercise #7 - Piglet

---
+++ Loops/Loops/Exercise7/Piglet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Piglet
+    {
+        static void Main(string[] args)
+        {
+            PlayPiglet();
+        }
+
+        static void PlayPiglet()
+        {
+            Random random = new Random();
+            bool endTrigger = new bool();
+            int currentRoll = 0;
+            int score = 0;
+
+            Console.WriteLine("Welcome to Piglet!");
+
+            while (!GameEnded(currentRoll) && !endTrigger)
+            {
+                currentRoll = random.Next(1, 6);
+                score += currentRoll;
+
+                if (GameEnded(currentRoll))
+                {
+                    score = 0;
+                    Console.WriteLine($"You rolled a {currentRoll}!");
+                    Console.WriteLine($"You got {score} points.");
+
+                    break;
+                }
+
+                Console.WriteLine($"You rolled a {currentRoll}!");
+
+                Console.Write("Roll again? ");
+                string choice = Console.ReadLine();
+
+                switch (choice.ToLower())
+                {
+                    default:
+                        continue;
+
+                    case "no":
+                        Console.WriteLine($"You got {score} points.");
+                        endTrigger = true;
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool GameEnded(int number)
+        {
+            return number == 1;
+        }
+    }
+}
