b797b5df0b7271c34e6f59344a74338df5f980fb
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-09 21:45:31 +0300

    Exercise #11 - ReverseTheCase

---
+++ Loops/Loops/Exercise11/ReverseTheCase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise11
+{
+    class ReverseTheCase
+    {
+        static void Main(string[] args)
+        {
+            ReverseCase("Happy Birthday");
+            ReverseCase("MANY THANKS");
+            ReverseCase("sPoNtAnEoUs");
+        }
+
+        static void ReverseCase(string input)
+        {
+            string result = "";
+
+            foreach (char c in input)
+            {
+                if (c.ToString().Equals(c.ToString().ToUpper()))
+                {
+                    result += c.ToString().ToLower();
+                }
+                else
+                {
+                    result += c.ToString().ToUpper();
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
