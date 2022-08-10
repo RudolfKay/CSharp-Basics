471ad5d0d41dd77d94637d086780d0896c588977
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-10 20:46:29 +0300

    Exercise #3 - FuelGaugeOdometer

---
+++ ClassesAndObjects/Exercise3/Odometer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Odometer
+    {
+        private int _mileage;
+        private int _fuelBurnRate;
+        private FuelGauge _fuel;
+
+        public Odometer(int startingMileage, FuelGauge fuel, int fuelBurnRate)
+        {
+            _mileage = startingMileage;
+            _fuel = fuel;
+            _fuelBurnRate = fuelBurnRate;
+        }
+
+        public int GetMileage()
+        {
+            return _mileage;
+        }
+
+        public void Increment()
+        {
+            int kmCount = 0;
+
+            while (_fuel.GetFuel() > 0)
+            {
+                if (kmCount % _fuelBurnRate == 0)
+                {
+                    Console.WriteLine($"Driving... {_fuel.GetFuel()}l remaining.");
+                    _fuel.Burn();
+                    Console.WriteLine($"You've driven {kmCount}km. Total mileage is {_mileage}km");
+                }
+
+                _mileage = _mileage < 999999 ? _mileage += 1 : _mileage = 0;
+                kmCount++;
+            }
+
+            if (_fuel.GetFuel() == 0)
+            {
+                Console.WriteLine("\nThe car ran out of fuel.");
+                Console.WriteLine($"You drove {kmCount}km. Your total mileage is now {_mileage}km");
+            }
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much fuel is in your car? ");
+            double liters = Convert.ToDouble(Console.ReadLine());
+            double fuelMax = 70; // in liters
+            int fuelConsumption = 10; // km/l
+
+            FuelGauge testFuel = new FuelGauge(liters, fuelMax);
+            
+            Console.Write("What's the mileage? ");
+            int mileage = Convert.ToInt32(Console.ReadLine());
+
+            Odometer testOdo = new Odometer(mileage, testFuel, fuelConsumption);
+
+            testFuel.FillUp();
+            testOdo.Increment();
+
+            Console.ReadKey();
+        }
+    }
+}
