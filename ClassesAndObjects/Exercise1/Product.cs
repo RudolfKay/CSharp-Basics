1a56c6da0257ae538ecd507e75ec62e21bfab6eb
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-10 16:44:13 +0300

    Exercise #1 - Product

---
+++ ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Product
+    {
+        private string name;
+        private double price;
+        private int amount;
+
+        public Product(string productName, double priceAtStart, int amountAtStart)
+        {
+            name = productName;
+            price = priceAtStart;
+            amount = amountAtStart;
+        }
+
+        public void SetPrice(double newPrice)
+        {
+            price = newPrice;
+        }
+
+        public void SetAmount(int newAmount)
+        {
+            amount = newAmount;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{name}, price {price:0.00}, amount {amount}");
+        }
+    }
+}
