224b005f03eeaa89eb755706f73a70916c6ce50a
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-14 23:40:33 +0300

    Exercise #11 - Account

--- ClassesAndObjects/Account/Program.cs
+++ ClassesAndObjects/Account/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Account
 {
@@ -10,6 +7,33 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            CultureInfo enUS = new CultureInfo("en-US");
+
+            Account A = new Account("A", 100, enUS);
+            Account B = new Account("B", 0, enUS);
+            Account C = new Account("C", 0, enUS);
+
+
+            Console.WriteLine("Initial state");
+            Console.WriteLine($"{A}");
+            Console.WriteLine($"{B}");
+            Console.WriteLine($"{C}\n");
+
+            Transfer(A,B,50);
+            Transfer(B,C,25);
+
+            Console.WriteLine("Final state");
+            Console.WriteLine($"{A}");
+            Console.WriteLine($"{B}");
+            Console.WriteLine($"{C}");
+
+            Console.ReadKey();
+        }
+
+        public static void Transfer(Account from, Account to, double howMuch)
+        {
+            from.Withdrawal(howMuch);
+            to.Deposit(howMuch);
         }
     }
 }
