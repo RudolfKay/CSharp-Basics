e305a931a57bef8a59342c60ac43ff4b8603cf6d
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-15 20:34:17 +0300

    Exercise #14 - DutchDate

---
+++ ClassesAndObjects/Exercise14/Date.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise14
+{
+    public class Date
+    {
+        private CultureInfo _dutchCi;
+        private DateTime _dateTime;
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date()
+        {
+            _dutchCi = new CultureInfo("nl-NL");
+        }
+
+        public void WeekdayInDutch(string date)
+        {
+            _year = int.Parse(date.Split('/')[0]);
+            _month = int.Parse(date.Split('/')[1]);
+            _day = int.Parse(date.Split('/')[2]);
+
+            _dateTime = new DateTime(_year, _month, _day);
+
+            Console.WriteLine(_dateTime.ToString("dddd", _dutchCi));
+        }
+    }
+}
