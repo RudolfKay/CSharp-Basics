17968fa0d84521c8863fc7fb5b31e748898d15f8
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-15 20:07:42 +0300

    Exercise #13 - Smoothie

---
+++ ClassesAndObjects/Exercise13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Smoothie s1 = new Smoothie(new string[] { "Banana" });
+            Console.WriteLine(s1.Ingredients); // ➞ { "Banana" }
+
+            Console.WriteLine(s1.GetCost()); // ➞ "£0.50"
+            Console.WriteLine(s1.GetPrice()); // ➞ "£1.25"
+            Console.WriteLine(s1.GetName()); // ➞ "Banana Smoothie"
+
+            Smoothie s2 = new Smoothie(new string[] { "Raspberries", "Strawberries", "Blueberries" });
+            Console.WriteLine(s2.Ingredients); // ➞ { "Raspberries", "Strawberries", "Blueberries" }
+
+            Console.WriteLine(s2.GetCost()); // ➞ "£3.50"
+            Console.WriteLine(s2.GetPrice()); // ➞ "£8.75"
+            Console.WriteLine(s2.GetName()); // ➞ "Blueberry Raspberry Strawberry Fusion";
+        
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise13/Smoothie.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise13
+{
+    public class Smoothie
+    {
+        private readonly string[] _ingredients;
+        private string _name;
+        private decimal _price;
+        private decimal _cost;
+
+        public Smoothie(string[] ingredients)
+        {
+            _ingredients = ingredients;
+        }
+
+        public string Ingredients => string.Join(", ", _ingredients);
+
+        public string GetName()
+        {
+            Array.Sort(_ingredients);
+            string name = "";
+
+            for (int i = 0; i < _ingredients.Length; i++)
+            {
+                if (_ingredients[i].EndsWith("ies"))
+                {
+                    name += _ingredients[i].Replace("ies", "y")+" ";
+                }
+                else
+                {
+                    name += _ingredients[i]+" ";
+                }
+            }
+
+            if (_ingredients.Length > 1)
+            {
+                name += $"Fusion";
+            }
+            else if (_ingredients.Length == 1)
+            {
+                name += $"Smoothie";
+            }
+
+            _name = name;
+
+            return _name;
+        }
+
+        public string GetPrice()
+        {
+            CultureInfo uk = new CultureInfo("en-GB");
+
+            decimal price = _cost + (_cost * (decimal)1.5);
+            _price = price;
+
+            return _price.ToString("C", uk);
+        }
+
+        public string GetCost()
+        {
+            CultureInfo uk = new CultureInfo("en-GB");
+            decimal cost = 0;
+
+            for (int i = 0; i < _ingredients.Length; i++)
+            {
+                string currentIngredient = _ingredients[i];
+
+                switch (currentIngredient)
+                {
+                    case "Strawberries":
+                        cost += (decimal)1.50;
+                        continue;
+
+                    case "Banana":
+                        cost += (decimal)0.50;
+                        continue;
+
+                    case "Mango":
+                        cost += (decimal)2.50;
+                        continue;
+
+                    case "Blueberries":
+                    case "Raspberries":
+                        cost += (decimal)1.00;
+                        continue;
+
+                    case "Apple":
+                        cost += (decimal)1.75;
+                        continue;
+
+                    case "Pineapple":
+                        cost += (decimal)3.50;
+                        continue;
+
+                    default:
+                        continue;
+                }
+            }
+
+            _cost = cost;
+
+            return _cost.ToString("C", uk);
+        }
+    }
+}
