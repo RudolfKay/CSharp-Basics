02cd61bb303aeac20a455900f561aa76584e9fa9
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-08-17 20:24:01 +0300

    Exercise #4 - UniqueNames

---
+++ Collections/Exercise4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<String> nameSet = new HashSet<String>();
+
+            string input = "";
+
+            while (nameSet.Count == 0 || !input.Trim().Equals(String.Empty))
+            {
+                Console.Write("Enter Name: ");
+                input = Console.ReadLine();
+                nameSet.Add(input);
+            }
+
+            Console.Write($"Unique name list contains: {string.Join(", ", nameSet).Trim(new char[] {',',' '})}.");
+            Console.ReadKey();
+        }
+    }
+}
